c2f1e4f3d66a9ea3bb0cb6691ca741162697ce03
Sumitx5 <126954034+Sumit282698@users.noreply.github.com>
2026-09-06 15:44:54 +0530

    Update 1.3.7

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.sumit.muzixx"
         minSdk = 24
         targetSdk = 36
-        versionCode = 26
-        versionName = "1.3.6"
+        versionCode = 27
+        versionName = "1.3.7"
         multiDexEnabled = true
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
